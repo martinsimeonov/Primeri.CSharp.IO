82e40bbe499baaab8d11ce76d93519432e23ea2c
Мартин Симеонов <simeonov.martin@gmail.com>
2015-07-25 10:04:21 +0300

    Проверка на съществуващ път

--- IOTesting/IOTextFiles/IOSettings.cs
+++ IOTesting/IOTextFiles/IOSettings.cs
@@ -47,13 +47,21 @@ namespace IOTextFiles
 		{
 			try
 			{
-				string _temp = System.IO.File.ReadAllText ( getPath () );
+				string _temp = "", _filePath = getPath ();
 
-				string[] _table = _temp.Replace("\r", "").Split ('\n');
-
-				for ( int i = 0; i < _table.Length; i++)
+				if ( System.IO.File.Exists ( _filePath) )	//Проверка дали пътят е валиден
 				{
-					_stable.stable[i] = _table[i];
+					System.IO.File.ReadAllText ( _filePath );
+
+					string[] _table = _temp.Replace("\r", "").Split ('\n');
+
+					for ( int i = 0; i < _table.Length; i++)
+					{
+						_stable.stable[i] = _table[i];
+					}
+				}else{
+					Console.WriteLine ("Не е намерен такъв път.");
+					return false;
 				}
 
 				return true;
